11166f09ee1b6579f75ea31ab81fbefc29b53b65
Yevhenii <iledevgeniy@gmail.com>
2019-10-03 00:29:05 +0300

    Code improving

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 let money = +prompt ("How much is your monthly budget?"),
     time = prompt ("Enter the date using format YYYY-MM-DD");
 
-
-var question1 = prompt ("Enter a required cost item this month"),
-    question2 = +prompt ("How much will it cost?"),
-    question3 = prompt ("Enter a required cost item this month"),
-    question4 = +prompt ("How much will it cost?");
-
 let appData = {
     budget: money,
     timeData: time,
-    expenses: { 
-        [question1]: question2,
-        [question3]: question4
-    },
+    expenses: {},
     optionalExpenses: {},
     income: [],
+    timeData: time,
     savings: false
 };
-alert((appData.budget - question2 - question4)/30);
+
+let question1 = prompt ("Enter a required cost item this month", ''),
+    question2 = +prompt ("How much will it cost?", ''),
+    question3 = prompt ("Enter a required cost item this month", ''),
+    question4 = +prompt ("How much will it cost?", '');
+
+appData.expenses.question1 = question2;
+appData.expenses.question3 = question4;
+
+alert(appData.budget /30);
